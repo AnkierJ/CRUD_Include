0332204b186b88246d077e87ceae70d9f8ade756
Pedro <pedro.henriquesantos1407@gmail.com>
2024-09-02 17:02:40 -0300

    Atividades sendo filtradas corretamente nas colunas.

--- FrontEnd_Pedro_1/src/App.tsx
+++ FrontEnd_Pedro_1/src/App.tsx
@@ -1,11 +1,8 @@
 import './App.css';
-import { useAtividadeData } from './hooks/useAtividadeData';
-import { Card } from './components/card/card';
 import { Header } from './components/Header';
 import { Board } from './components/Board';
 
 function App() {
-  const { data } = useAtividadeData();
 
   return (
     <div className="container">
@@ -13,7 +10,7 @@ function App() {
       <Board />
 
       <div className="card-grid">
-        {data?.map((atividade) => (
+      {/*data?.map((atividade) => (
           <Card
             key={atividade.id} // Chave única para cada componente Card
             id={atividade.id}
@@ -21,8 +18,9 @@ function App() {
             description={atividade.description} 
             date={new Date(atividade.date)} // Convertendo string para objeto Date, se necessário
           />
-        ))}
-        </div>
+      ))*/}
+
+      </div>
 
       <footer className="app-footer">
         <p>&copy; 2024 - Meu Gerenciador de Tarefas</p>
--- FrontEnd_Pedro_1/src/components/formatDate.ts
+++ FrontEnd_Pedro_1/src/components/formatDate.ts
@@ -1,5 +1,3 @@
-// src/components/formatDate.ts
-
 export function formatDate(date: Date): string {
     return date.toLocaleDateString('pt-BR', {
       day: '2-digit',
