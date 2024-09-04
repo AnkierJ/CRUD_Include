887aa0fd7b86aa0daf20609146a86f52244f9a0c
AnkierJ <ankierj@gmail.com>
2024-09-04 17:38:14 -0300

    adição dos botões, inclusão da data limite e mais melhorias

--- FrontEnd_Pedro_1/src/components/Column.tsx
+++ FrontEnd_Pedro_1/src/components/Column.tsx
@@ -22,14 +22,20 @@ export function Column({ column_id, title, tasks, onAddTask}: ColumnProps) {
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setColumnTitle(event.target.value);
   };
-
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleAddTask()
+      setIsEditing(false)
+    }
+  };
   const handleAddTask = () => {
     if (newTaskTitle.trim()) {
       const newTask = {
         id: Date.now(),
         title: newTaskTitle,
-        description: 'Descrição padrão',
+        description: '',
         date: new Date(),
+        deadline: '',
         column_id: columnId // Adicionando o ID da coluna à nova tarefa
       };
       onAddTask(newTask);
@@ -38,77 +44,70 @@ export function Column({ column_id, title, tasks, onAddTask}: ColumnProps) {
   };
 
   return (
-    <div className="column" style={{height: isMinimized? '10%':'100%'}}>
-      {isMinimized ? (
-        <div className="column-header">
-          {isEditing ? (
-            <>
-              <input
-                type="text"
-                value={columnTitle}
-                onChange={handleTitleChange}
-                onBlur={() => setIsEditing(false)}
-                autoFocus
-              />
-            </>
-          ) : (
-            <h2 onClick={() => setIsEditing(true)}>{columnTitle}</h2>
-          )}
-          {/*<p>{Array.prototype.filter.call(cardgrid, elemento => elemento.classList.contains(Card)).length}</p>*/}
+    <div className="column" style={{ height: isMinimized ? "10%" : "100%" }}>
+    <>
+      <div className="column-header">
+        {isEditing ? (
+          <>
+            <input
+              type="text"
+              value={columnTitle}
+              onChange={handleTitleChange}
+              onBlur={() => setIsEditing(false)}
+              autoFocus
+              onKeyDown={handleKeyDown}
+            />
+          </>
+        ) : (
+          <h2 onClick={() => setIsEditing(true)}>{columnTitle}</h2>
+        )}
+        {isMinimized ? (
           <button
             className="minimize-column"
             onClick={() => setIsMinimized(false)}
           >
             <AiFillCaretDown color="Grey" />
           </button>
-        </div>
-      ) : (
-        <>
-          <div className="column-header">
-            {isEditing ? (
-              <>
-                <input
-                  type="text"
-                  value={columnTitle}
-                  onChange={handleTitleChange}
-                  onBlur={() => setIsEditing(false)}
-                  autoFocus
-                />
-              </>
-            ) : (
-              <h2 onClick={() => setIsEditing(true)}>{columnTitle}</h2>
-            )}
-            <button
-              className="minimize-column"
-              onClick={() => setIsMinimized(true)}
-            >
-              <AiFillCaretUp color="Grey" />
-            </button>
-          </div>
-          <div className="add-task">
-            <input
-              type="text"
-              placeholder="Título da tarefa"
-              value={newTaskTitle}
-              onChange={(e) => setNewTaskTitle(e.target.value)}
-            />
-            <button className="add-task-button" onClick={handleAddTask}>
-              Adicionar Tarefa
-            </button>
-          </div>
-          <div className="card-grid">
-            {tasks.map((atividade) => (
-              <Card
-                key={atividade.id} // Chave única para cada componente Card
-                id={atividade.id}
-                title={atividade.title}
-                description={atividade.description}
-                date={new Date(atividade.date)} // Convertendo string para objeto Date, se necessário
-              />
-            ))}
-          </div>
-        </>
-      )}
-    </div>
+        ) : (
+          <button
+            className="minimize-column"
+            onClick={() => setIsMinimized(true)}
+          >
+            <AiFillCaretUp color="Grey" />
+          </button>
+        )}
+      </div>
+      <div
+        className="add-task"
+        style={{ display: isMinimized ? "none" : "flex" }}
+      >
+        <input
+          type="text"
+          placeholder="Título da tarefa"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button className="add-task-button" onClick={handleAddTask}>
+          Adicionar Tarefa
+        </button>
+      </div>
+      <div
+        className="card-grid"
+        style={{ display: isMinimized ? "none" : "flex" }}
+      >
+        {tasks.map((atividade) => (
+          <Card
+            key={atividade.id} // Chave única para cada componente Card
+            id={atividade.id}
+            title={atividade.title}
+            description={atividade.description}
+            date={new Date(atividade.date)} // Convertendo string para objeto Date, se necessário
+            columnId={columnId}
+          />
+        ))}
+      </div>
+    </>
+  </div>
   );
 }
--- FrontEnd_Pedro_1/src/components/card/card.tsx
+++ FrontEnd_Pedro_1/src/components/card/card.tsx
@@ -2,33 +2,47 @@ import React from "react";
 import { useState } from "react";
 import "./Card.css";
 import { CirclePicker } from "react-color";
-import { AiFillTag, AiFillEdit, AiOutlineCheckCircle } from "react-icons/ai";
+import {
+  AiFillTag,
+  AiFillEdit,
+  AiOutlineCheckCircle,
+  AiFillCloseCircle,
+  AiOutlineLeft,
+  AiOutlineRight,
+} from "react-icons/ai";
 
 interface CardProps {
   id: number;
   title: string;
   description: string;
   date: Date;
+  deadline: Date;
+  columnId: number;
 }
 
-export function Card({ title, description, date }: CardProps) {
+export function Card({ title, description, date, deadline, columnId }: CardProps) {
   const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit'
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
   };
   const [currentColor, setCurrentColor] = useState("grey");
   const [isEditing, setIsEditing] = useState(false);
   const [isTagging, setIsTagging] = useState(false);
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
-  const [newDate, setNewDate] = useState(date);
+  const [newDeadline, setNewDeadline] = useState(deadline);
   const handleSave = () => {
     setIsEditing(false);
     setIsTagging(false);
     // Aqui você pode enviar as atualizações para o backend, se necessário.
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSave();
+    }
   };
   const handleChangeComplete = (color: any) => {
     setCurrentColor(color.hex);
@@ -36,15 +50,17 @@ export function Card({ title, description, date }: CardProps) {
   };
 
   return (
-    <div className="card" style={{ borderLeft: `4px solid ${currentColor}` }} draggable='true'>
+    <div className="card" style={{ borderLeft: `4px solid ${currentColor}` }}>
       {isEditing ? (
         <div className="editContainer">
           <input
             required
-            style={{ fontSize: "14px", fontWeight:'bold' }}
+            style={{ fontSize: "14px", fontWeight: "bold" }}
             type="text"
+            placeholder="Título"
             value={newTitle}
             onChange={(e) => setNewTitle(e.target.value)}
+            onKeyDown={handleKeyDown}
           />
           <input
             style={{ height: "32px" }}
@@ -52,12 +68,15 @@ export function Card({ title, description, date }: CardProps) {
             type="text"
             value={newDescription}
             onChange={(e) => setNewDescription(e.target.value)}
+            onKeyDown={handleKeyDown}
           />
+          <p style={{margin:'0', fontSize:14, justifySelf: 'left'}}>Data limite:</p>
           <input
             style={{ padding: "4px" }}
-            type="date"
-            value={newDate.toISOString().split("T")[0]}
-            onChange={(e) => setNewDate(new Date(e.target.value))}
+            type="datetime-local"
+            value={date.toISOString().split("T")[0]}
+            onChange={(e) => setNewDeadline(new Date(e.target.value))}
+            onKeyDown={handleKeyDown}
           />
           <button onClick={handleSave} className="saveButton">
             <AiOutlineCheckCircle
@@ -70,56 +89,77 @@ export function Card({ title, description, date }: CardProps) {
         </div>
       ) : (
         <div className="infoContainer">
-          <button onClick={() => setIsEditing(true)} className="editButton">
-            <AiFillEdit color="grey" size={16} />
-          </button>
-          <h4 style={{ marginBlock: 0 }}>
-            {newTitle}
-          </h4>
-          <p
-            className="card-description"
-            style={{ marginBlock:4}}
-          >
-            {newDescription}
-          </p>
-          <p style={{ fontSize: 14, marginBlock:0, color:'gray' }}>
-            {newDate.toLocaleTimeString('pt-BR', options)}
-          </p>
-        </div>
-      )}
-      {isTagging ? (
-        <div className="colorPicker">
-          <CirclePicker
-            width="240px"
-            color={currentColor}
-            onChangeComplete={handleChangeComplete}
-            circleSize={20}
-            circleSpacing={10}
-            colors={[
-              "red",
-              "#f44336",
-              "#e91e63",
-              "#9c27b0",
-              "#673ab7",
-              "#3f51b5",
-              "#2196f3",
-              "#00bcd4",
-              "#009688",
-              "#4caf50",
-              "#8bc34a",
-              "#ffeb3b",
-              "#ffc107",
-              "#ff9800",
-              "#795548",
-              "grey",
-            ]}
-          />
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => setIsTagging(true)} className="tagButton">
-            <AiFillTag color="grey" size={16} />
-          </button>
+          <div className="buttons">
+            <button onClick={() => setIsEditing(true)}>
+              <AiFillEdit color="grey" size={16} />
+            </button>
+            <button onClick={() => setIsTagging(true)}>
+              <AiFillTag color="grey" size={16} />
+            </button>
+            <button>
+              <AiFillCloseCircle color="grey" size={16} />
+            </button>
+          </div>
+        
+          <h4 style={{ marginBlock: 0 }}>{newTitle}</h4>
+          {newDescription && <p className="card-description" style={{ marginBlock: 4 }}>
+            {newDescription} 
+          </p>}
+          {!newDescription && <p className="card-description" style={{ marginBlock: 4, color:'grey'}}>
+            Edite para criar uma descrição! 
+          </p>}
+          {!newDeadline && <p style={{ fontSize: 12, marginBlock: 0, color: "gray" }}>
+            <strong>Criado:</strong> {date.toLocaleTimeString("pt-BR", options)}
+          </p>}
+          {newDeadline && <p style={{ fontSize: 12, marginBlock: 0, color: "gray" }}>
+            <strong>Criado:</strong> {date.toLocaleTimeString("pt-BR", options)} <strong>Até:</strong> {newDeadline.toLocaleDateString("pt-BR", options)}
+          </p>}
+
+          {isTagging ? (
+            <div className="colorPicker">
+              <CirclePicker
+                width="240px"
+                color={currentColor}
+                onChangeComplete={handleChangeComplete}
+                circleSize={20}
+                circleSpacing={10}
+                colors={[
+                  "red",
+                  "#f44336",
+                  "#e91e63",
+                  "#9c27b0",
+                  "#673ab7",
+                  "#3f51b5",
+                  "#2196f3",
+                  "#00bcd4",
+                  "#009688",
+                  "#4caf50",
+                  "#8bc34a",
+                  "#ffeb3b",
+                  "#ffc107",
+                  "#ff9800",
+                  "#795548",
+                  "grey",
+                ]}
+              />
+            </div>
+          ) : null}
+          <div className="navarrows">
+            <button
+              style={columnId == 1 ? { display: "none" } : { display: "grid" }}
+            >
+              <AiOutlineLeft color="grey" size={16} />
+            </button>
+            <button>
+              <AiOutlineRight
+                color="grey"
+                size={16}
+                style={
+                  columnId == 3 ? { display: "none" } : { display: "grid" }
+                }
+              />
+            </button>
+          </div>
         </div>
       )}
     </div>
